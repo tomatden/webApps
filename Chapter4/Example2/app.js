c95e0962b95d547509774ab2bb5e11c4040038c2
Tom Peters <peterst@m16785.pinnacol.com>
2014-04-25 16:39:51 -0600

    intial commit of chap 4 files

---
+++ Chapter4/Example2/app.js
@@ -0,0 +1,10 @@
+var main = function () {
+    "use strict";
+
+    $(".comment-input button").on("click", function (event) {
+        console.log("hello world");
+    });
+
+};
+
+$(document).ready(main);
