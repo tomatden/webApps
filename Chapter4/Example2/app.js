80db23955094f2022858c8ca90fd98f226ee55a8
Tom <tomatden@elguapo.(none)>
2014-04-28 12:11:00 -0600

    changes

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,10 +1,27 @@
 var main = function () {
     "use strict";
 
+    var addCommentFromInputBox = function (){
+    	var $new_comment = $("<p>");
+    	var comment_text = $(".comment-input input").val();
+    	if (comment_text !== "") {
+	    	$new_comment.text(comment_text);
+	    	$new_comment.hide();
+    	    $(".comments").append($new_comment);
+    	    $new_comment.fadeIn();
+    	    comment_text = "";
+    	    $(".comment-input input").val(comment_text);
+    	} 
+    };
+
     $(".comment-input button").on("click", function (event) {
-    	var $new_comment = $("<p>");;
-    	$new_comment.text($(".comment-input input").val());
-        $(".comments").append($new_comment);
+    	addCommentFromInputBox();
+    });
+
+    $(".comment-input input").on("keypress",function (event) {
+    	if (event.keyCode === 13){
+    		addCommentFromInputBox();
+    	}
     });
 
 };
