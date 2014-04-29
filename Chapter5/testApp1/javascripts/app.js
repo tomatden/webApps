1039c27a77a8026646dd1abefd470842002225b5
Tom Peters <peterst@m16785.pinnacol.com>
2014-04-29 17:08:21 -0600

    chap 5 more stuff

--- Chapter5/testApp1/javascripts/app.js
+++ Chapter5/testApp1/javascripts/app.js
@@ -1,7 +1,24 @@
 var main = function () {
 	"use strict";
 	
-	window.alert("hello world!");
+	console.log("hello world!");
+	
+	$.getJSON("cards/aceOfSpades.json",function (card) {
+		var $cardParagraph = $("<p>");
+		$cardParagraph.text(card.rank + " of " + card.suit);
+		$("main").append($cardParagraph);
+	});
+	$.getJSON("cards/hand.json",function (hand) {
+		var $list = $("<ul>");
+		hand.forEach(function (card) {
+			var $card = $("<li>");
+			$card.text(card.rank + " of " + card.suit);
+			console.log(JSON.stringify(card));
+			$list.append($card);
+		});
+		$("main").append($list);
+	});
+	
 };
 
 $(document).ready(main);
